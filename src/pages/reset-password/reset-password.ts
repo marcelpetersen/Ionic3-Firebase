8ab1522305a4cb6568d7a29c65ded0aaff81edec
Korakoch Singkam <korakoch@toworksmile.com>
2017-05-31 15:39:36 +0700

    Complete reset password.

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, LoadingController, AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Validator } from "../../libs/util";
+import firebase from "firebase";
 
 /**
  * Generated class for the ResetPasswordPage page.
@@ -13,12 +16,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'reset-password.html',
 })
 export class ResetPasswordPage {
+  public resetPasswordForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public loadingCtrl: LoadingController, public alertCtrl: AlertController) {
+
+    this.resetPasswordForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validator.email])]
+    });
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResetPasswordPage');
   }
 
+  resetPassword() {
+    if (!this.resetPasswordForm.valid) {
+      console.log(this.resetPasswordForm.value);
+    } else {
+      firebase.auth().sendPasswordResetEmail(this.resetPasswordForm.value.email)
+        .then((user) => {
+          let alert = this.alertCtrl.create({
+            message: "We just sent you a reset link to your email",
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel',
+                handler: () => {
+                  this.navCtrl.pop();
+                }
+              }
+            ]
+          }); alert.present();
+        }, (error) => {
+          let errorMessage: string = error.message;
+          let errorAlert = this.alertCtrl.create({
+            message: errorMessage,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          errorAlert.present();
+        });
+    }
+  }
 }
